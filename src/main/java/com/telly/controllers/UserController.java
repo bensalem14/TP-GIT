381afadc396a46b0d99860cd6e6ecfa3da8e5cc1
bensalem14 <104575447+bensalem14@users.noreply.github.com>
2023-01-08 20:30:10 +0100

    Déconnexion

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -30,7 +30,10 @@ public class UserController {
 	public String showLogin() {
 		return "login";
 	}
-
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
 	
 
 }
